086983cf8f0f6c471b3762bec3e70aee119cce53
Michael Kovacsik <kovacsikdev@gmail.com>
2025-02-04 18:28:11 -0800

    blank miniprojects for now

--- src/app/miniprojects/page.tsx
+++ src/app/miniprojects/page.tsx
@@ -1,57 +1,25 @@
 "use client";
 
-import React, { useState } from "react";
-import Image from "next/image";
-import InfoIcon from "../../assets/info-icon.svg";
-
-import { DevInfo } from "@/components";
+// import { DevInfo } from "@/components";
+// import { Tile } from "@/components/miniprojects";
+// import TileGame from "@/components/miniprojects/tilegame/TileGame";
+// import WeatherApp from "@/components/miniprojects/weatherapp/WeatherApp";
+// import Videos from "@/components/miniprojects/videos/Videos";
 
 import "./page.css";
 
 const MiniProjects = () => {
-  const [displayDevNote, setDisplayDevNote] = useState(false);
-  const [displayMiniProjectItem, setDisplayMiniProjectItem] = useState(false);
-
   return (
     <>
-      <div className="page-title text-4x1">
+      {/* <div className="page-title text-4xl">
         <h2>Mini Projects</h2>
-        <button
-          onClick={() => {
-            setDisplayDevNote(true);
-          }}
-        >
-          <Image
-            style={{ margin: "0 0.5rem" }}
-            src={InfoIcon}
-            alt="information icon"
-          />
-        </button>
-        {displayDevNote && (
-          <DevInfo
-            toggle={() => {
-              setDisplayDevNote(false);
-            }}
-          >
-            Mini projects
-          </DevInfo>
-        )}
+        <DevInfo>Mini Projects</DevInfo>
       </div>
-      <div className="flex flex-wrap">
-        <div className="mini-project-item-wrapper border border-gray-600 rounded-lg m-3">
-          <div
-            className={`mini-project-item ${
-              displayMiniProjectItem ? "display" : ""
-            } flex justify-center items-center border border-gray-600 rounded-lg p-3`}
-            onClick={() => {
-              setDisplayMiniProjectItem(!displayMiniProjectItem);
-            }}
-          ></div>
-        </div>
-        <div className="h-[480px] w-[480px] flex justify-center items-center border border-gray-600 rounded-lg p-3 m-3"></div>
-        <div className="h-[480px] w-[480px] flex justify-center items-center border border-gray-600 rounded-lg p-3 m-3"></div>
-        <div className="h-[480px] w-[480px] flex justify-center items-center border border-gray-600 rounded-lg p-3 m-3"></div>
-      </div>
+      <div className="flex flex-wrap relative">
+        <Tile thumbnail={"Tile game"}><TileGame /></Tile>
+        <Tile thumbnail={"Weather app"}><WeatherApp /></Tile>
+        <Tile thumbnail={"Videos"}><Videos /></Tile>
+      </div> */}
     </>
   );
 };
